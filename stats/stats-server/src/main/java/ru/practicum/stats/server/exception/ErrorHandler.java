b86bbe114cf65453bc07645ec06061c76b653f45
EgerV1d <Energizer122@yandex.ru>
2026-08-18 13:25:21 +0300

    Исправление для тестов

--- stats/stats-server/src/main/java/ru/practicum/stats/server/exception/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/exception/ErrorHandler.java
@@ -1,17 +1,24 @@
 package ru.practicum.stats.server.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleIllegalArgument(IllegalArgumentException e) {
         return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMissingParams(MissingServletRequestParameterException e) {
+        return Map.of("error", "Required parameter '" + e.getParameterName() + "' is missing");
     }
 }
